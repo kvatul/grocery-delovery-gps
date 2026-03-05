beeaa71032876101b09bfecdc4f51ae5d39be907
Atul Kumar <kvatul15@gmail.com>
2026-03-05 18:06:36 +0530

    ind4

--- snapCart/src/lib/socket.ts
+++ snapCart/src/lib/socket.ts
@@ -4,7 +4,11 @@ let socket: Socket | null = null;
 export const getSocket = () => {
   //console.log("localhost", process.env.NEXT_SOCKET_URL);
   if (!socket) {
-    socket = io("http://localhost:4000"); //process.env.NEXT_SOCKET_URL
+    //socket = io("http://localhost:4000"); //process.env.NEXT_SOCKET_URL
+    // socket = io(process.env.NEXT_SOCKET_URL); //process.env.NEXT_SOCKET_URL
+    const socket = io("https://socketioserver-at8m.onrender.com", {
+      transports: ["websocket"],
+    });
 
     socket.on("connect", () => {
       console.log("Connected to socket server");
